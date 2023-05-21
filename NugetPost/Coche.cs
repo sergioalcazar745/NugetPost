6753535549f1e94ab736dc0312fe89188a083555
Sergio Alcázar Monedero <60264314+sergioalcazar745@users.noreply.github.com>
2023-05-21 13:49:15 +0200

    Add project files.

---
+++ NugetPost/Coche.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetPost
+{
+    public class Coche
+    {
+        public int IdCoche { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Imagen { get; set; }
+    }
+}
---
+++ NugetPost/Garaje.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetPost
+{
+    public class Garaje
+    {
+        public List<Coche> GetCoches()
+        {
+            List<Coche> coches = new List<Coche>();
+            Coche c = new Coche
+            {
+                IdCoche = 0,
+                Marca = "DMG",
+                Modelo = "Delorian",
+                Imagen = "https://static.motor.es/fotos-noticias/2015/10/min652x435/curiosidades-delorean-regreso-al-futuro-201523728_4.jpg"
+            };
+            coches.Add(c);
+            c = new Coche
+            {
+                IdCoche = 1,
+                Marca = "PONTIAC"
+                ,
+                Modelo = "FireBird"
+                ,
+                Imagen = "https://i.ytimg.com/vi/UJFwmjfTSJw/hqdefault.jpg"
+            };
+            coches.Add(c);
+            c = new Coche
+            {
+                IdCoche = 2
+                ,
+                Marca = "Volkswagen"
+                ,
+                Modelo = "Escarabajo"
+                ,
+                Imagen = "https://i.ytimg.com/vi/AP-HLHi0HUw/maxresdefault.jpg"
+            };
+            coches.Add(c);
+            c = new Coche
+            {
+                IdCoche = 3
+                ,
+                Marca = "Citroen"
+                ,
+                Modelo = "2 CV"
+                ,
+                Imagen = "http://iconroad.es/onewebmedia/Iconos%20Citro%C3%ABn%202CV%20En%20el%20cine%204.png"
+            };
+            coches.Add(c);
+            return coches;
+        }
+
+        public Coche FindCoche(int idcoche)
+        {
+            return this.GetCoches()
+                .FirstOrDefault(z => z.IdCoche == idcoche);
+        }
+    }
+
+}
